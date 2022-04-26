b8730316ec9330acc9965767f25c56bc83c86508
Maciej Handke <maciejhandke11@gmail.com>
2022-04-26 20:58:57 +0200

    Code cleanup

--- stopwatch.py
+++ stopwatch.py
@@ -1,14 +1,20 @@
 import time
-import os
-
-was_timer_stopped = False
+from tkinter import *
 
 
 class StopwatchClass:
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
+        self.hours = 0
         self.minutes = 0
         self.seconds = 0
+        self.h = ''
+        self.m = ''
+        self.s = ''
         self.stop = False
+        self.stopwatch_content = Label(self.parent, text='00:00:00')
+        self.stopwatch_content.place(relx=0.5, rely=0.3, anchor=CENTER)
+        self.stopwatch_content.config(font=('Arial', 100), fg='#db7a67', bg='#494949', borderwidth=0)
 
     def stopwatch_start(self):
         while not self.stop:
@@ -16,15 +22,24 @@ class StopwatchClass:
             if self.seconds == 60:
                 self.minutes += 1
                 self.seconds = 0
+            if self.minutes == 60:
+                self.hours += 1
+                self.minutes = 0
             time.sleep(1)
-            if self.minutes < 10 and self.seconds < 10:
-                print(f'0{self.minutes}:0{self.seconds}')
-            elif self.minutes < 10 and self.seconds >= 10:
-                print(f'0{self.minutes}:{self.seconds}')
-            elif self.minutes > 10 and self.seconds < 10:
-                print(f'{self.minutes}:0{self.seconds}')
-            else:
-                print(f'{self.minutes}:{self.seconds}')
+            if self.hours < 10:
+                self.h = f'0{self.hours}'
+            elif self.hours >= 10:
+                self.h = f'{self.hours}'
+            if self.minutes < 10:
+                self.m = f'0{self.minutes}'
+            elif self.minutes >= 10:
+                self.m = f'{self.minutes}'
+            if self.seconds < 10:
+                self.s = f'0{self.seconds}'
+            elif self.seconds >= 10:
+                self.s = f'{self.seconds}'
+            self.stopwatch_content.config(text=f'{self.h}:{self.m}:{self.s}')
+            self.stopwatch_content.update()
 
     def stopwatch_stop(self):
         self.stop = True
@@ -35,6 +50,12 @@ class StopwatchClass:
 
     def stopwatch_reset(self):
         self.stop = False
+        self.hours = 0
         self.minutes = 0
         self.seconds = 0
-        print(f'00:00')
+        self.stopwatch_content.config(text=f'00:00:00')
+        self.stopwatch_content.update()
+
+
+
+
